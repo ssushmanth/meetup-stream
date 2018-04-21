2d0bd3c03156b466ec18920676f44f23322dae65
Sushmanth S <36072077+ssushmanth@users.noreply.github.com>
2018-04-21 19:55:36 +0530

    Update Dto.scala

--- src/main/scala/core/Dto.scala
+++ src/main/scala/core/Dto.scala
@@ -1,12 +1,11 @@
 package core
-  
-case class Venue(name: Option[String], address1: Option[String], city: Option[String], state: Option[String], zip: Option[String], country: Option[String], lon: Option[Float], lat: Option[Float]){
-  def toExpandedCityString=List(city, state, country).flatMap{x=>x.map{_.toLowerCase()}}.mkString(",")
-}
 
-case class Event(id: String, name: Option[String], eventUrl: Option[String], description: Option[String], time: Option[Long], rsvpLimit: Option[Int])
-case class Group(id: Option[String], category: Option[String], name: Option[String], city: Option[String], state: Option[String], country: Option[String])
+case class EventDetails(id: String, name: String, city: String, country: String, payment_required: Int, cat_id: Int, cat_name: String, duration: Long)
+ 
+case class Venue(name: Option[String], address1: Option[String], city: Option[String], state: Option[String], zip: Option[String], country: Option[String], lon: Option[Float], lat: Option[Float])
+case class Event(id: String, name: Option[String], eventUrl: Option[String], description: Option[String], duration: Option[Long], rsvpLimit: Option[Int], paymentRequired: Option[Int], status: Option[String])
+case class Group(id: Option[String], name: Option[String], city: Option[String], state: Option[String], country: Option[String])
+case class Category(name: Option[String], id: Option[Int], shortname: Option[String])
 
 case class Member(memberName: Option[String], memberId: Option[String])
 case class MemberEvent(eventId: Option[String], eventName: Option[String], eventUrl: Option[String], time: Option[Long])
-  
